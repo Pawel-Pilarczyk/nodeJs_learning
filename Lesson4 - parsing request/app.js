7d42b0ceeb0895a11f0136b1e9ec8c434a2df5a4
Pawel Pilarczyk <pawelpilarczyk1@gmail.com>
2023-02-02 09:00:33 +0100

    parsing request lesson

---
+++ Lesson4 - parsing request/app.js
@@ -0,0 +1,33 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write("<h1>Hello page 1</h1>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    res.end();
+  }
+  if (url === "/message" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.writeHead(302, { Location: "/" });
+    res.end();
+  }
+});
+
+server.listen(3000);
