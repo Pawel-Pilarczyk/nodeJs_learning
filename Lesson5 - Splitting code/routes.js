bd6dfa46a21603a25854717bfaf20d157e29a5be
Pawel Pilarczyk <pawelpilarczyk1@gmail.com>
2023-02-05 21:42:01 +0100

    Splitting the code

---
+++ Lesson5 - Splitting code/routes.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+
+function requestHandler(req, res) {
+  const url = req.url;
+
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write("<h1>Hello page 1</h1>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    res.end();
+  }
+
+  if (url === "/message" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.writeHead(302, { Location: "/" });
+    res.end();
+  }
+}
+
+module.exports = requestHandler;
